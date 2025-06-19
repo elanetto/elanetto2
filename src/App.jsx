15bce9c9a50b2cce92e63c9973a4bb58aacb3e69
elanetto <101054274+elanetto@users.noreply.github.com>
2025-06-19 20:43:03 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen w-full bg-pink-400 flex justify-center items-center flex-col">
+        <h1 className="text-5xl">Hello there</h1>
+        <p>Here we go</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
