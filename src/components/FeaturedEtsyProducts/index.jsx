ac0702052b682b0572d1142ad596293865bf23b9
elanetto <101054274+elanetto@users.noreply.github.com>
2025-06-24 22:23:02 +0200

    Edited product cards: Title

--- src/components/FeaturedEtsyProducts/index.jsx
+++ src/components/FeaturedEtsyProducts/index.jsx
@@ -6,22 +6,22 @@ import singlePlant from "./../../assets/etsy/single_sticker_26.png";
 export default function FeaturedEtsyProducts() {
   const products = [
     {
-      title: "Tea Time Sticker Sheet",
+      title: "Tea Time",
       image: teaTimeSheet,
       link: "https://www.etsy.com/no-en/listing/1410769233/cute-tea-time-sticker-sheet-bullet?ref=shop_home_feat_1&logging_key=1f88ec0a292d5f1eebeb9f37c055a2b4576d6a21%3A1410769233",
     },
     {
-      title: "Coffee Time Sticker Sheet",
+      title: "Coffee Time",
       image: coffeeSheet,
       link: "https://www.etsy.com/no-en/listing/1411321507/cute-coffee-sticker-sheet-bullet-journal?ref=shop_home_active_2&logging_key=26a2b53fac7a1b0d7357b630abc744dbae0e9a7a%3A1411321507",
     },
     {
-      title: "Plants Sticker Sheet",
+      title: "Plants",
       image: plantsSheet,
       link: "https://www.etsy.com/no-en/listing/1397134772/plants-sticker-sheet-bullet-journal?ref=shop_home_feat_3&logging_key=17864461157f34ad9e5ebc2e5b207aa881cac6f3%3A1397134772",
     },
     {
-      title: "Red Box Rage Sticker",
+      title: "Happy Plant",
       image: singlePlant,
       link: "https://www.etsy.com/no-en/listing/1640672814/happy-plant-die-cut-sticker-kawaii?ref=shop_home_feat_2&logging_key=a186f282838b0be6b0e0a032ad7ccff0d13e8612%3A1640672814",
     },
@@ -46,8 +46,8 @@ export default function FeaturedEtsyProducts() {
               alt={item.title}
               className="w-full h-48 object-cover"
             />
-            <div className="p-4">
-              <h3 className="font-semibold text-pink-800 text-sm text-center">
+            <div className="bg-dirtyrosa w-full h-full py-2">
+              <h3 className="font-semibold text-white text-sm text-center">
                 {item.title}
               </h3>
             </div>
